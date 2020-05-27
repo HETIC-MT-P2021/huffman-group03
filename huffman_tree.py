1f1965c3316935d55c423035db439bd7f9509810
Edwin Vautier <edwin.vautier@gmail.com>
2020-05-27 22:08:18 +0200

    :recycle: Refactored code with functions and add some comments to make it more readable.

--- huffman_tree.py
+++ huffman_tree.py
@@ -1,68 +1,82 @@
 import sys
 import operator
 
-# Read command args to get filename to read
-filename = sys.argv[1]
+def get_file_content():
+    # Read command args to get filename to read
+    filename = sys.argv[1]
 
-f = open(filename, "r")
-text = f.read()
-f.close()
+    f = open(filename, "r")
+    text = f.read()
+    f.close()
+
+    return text
 
 # ---------------- Count symbols occurences -----------------
 
-occurences = dict()
-for symbol in text:
-    if symbol in occurences.keys():
-        occurences[symbol] += 1
-    else:
-        occurences[symbol] = 1
+def count_and_sort_occurences(text):
+    occurences = dict()
+    for symbol in text:
+        if symbol in occurences.keys():
+            occurences[symbol] += 1
+        else:
+            occurences[symbol] = 1
 
-sorted_occurences = sorted(occurences.items(), key=operator.itemgetter(1))
+    return sorted(occurences.items(), key=operator.itemgetter(1))
 
-# ---------------- Create Huffman tree -----------------
+# ---------------- Huffman tree node class -----------------
 class Node:
     def __init__(self, weight, left, right):
         self.weight = weight
         self.left = left
         self.right = right
     def __str__(self):
-        return "w:" + str(self.weight) + " - 0:\n\t<< " + str(self.left) + " >>\n- 1:\n\t<<" + str(self.right) + " >>"
+        return "w:" + str(self.weight) + " - 0:\n<< " + str(self.left) + " >>\n- 1:\n<<" + str(self.right) + " >>"
 
+# ---------------- Combine two elements into a node -----------------
+def create_node(left_child, right_child):
+    if type(left_child) != tuple and type(right_child) != tuple:
+        return Node(left_child.weight + right_child.weight, left_child, right_child)
+    elif type(left_child) == tuple and type(right_child) == tuple:
+        return Node(left_child[1] + right_child[1], left_child, right_child)
+    elif type(left_child) == tuple and type(right_child) != tuple:
+        return Node(left_child[1] + right_child.weight, left_child, right_child)
+    else:
+        return Node(left_child.weight + right_child[1], left_child, right_child)
 
-node = None
-while len(sorted_occurences) > 1:
-
-    # Pop and combine first two elements into a node
-    next_sorted_l = sorted_occurences.pop(0)
-    next_sorted_r = sorted_occurences.pop(0)
-
-    if type(next_sorted_l) != tuple and type(next_sorted_r) != tuple:
-        node = Node(next_sorted_l.weight + next_sorted_r.weight, next_sorted_l, next_sorted_r)
-    elif type(next_sorted_l) == tuple and type(next_sorted_r) == tuple:
-        node = Node(next_sorted_l[1] + next_sorted_r[1], next_sorted_l, next_sorted_r)
-    elif type(next_sorted_l) == tuple and type(next_sorted_r) != tuple:
-        node = Node(next_sorted_l[1] + next_sorted_r.weight, next_sorted_l, next_sorted_r)
-    elif type(next_sorted_l) != tuple and type(next_sorted_r) == tuple:
-        node = Node(next_sorted_l.weight + next_sorted_r[1], next_sorted_l, next_sorted_r)
-
-
-    # Append node to the occurences list at the right position
-
+# ---------------- Insert node in list depending on its weight -----------------
+ 
+def insert_node(node, occurences):
     i = 0
-    while i < len(sorted_occurences):
-        if type(sorted_occurences[i]) != tuple and node.weight < sorted_occurences[i].weight:
-            sorted_occurences.insert(i, node)
-            node = None
+    while i < len(occurences):
+        if type(occurences[i]) != tuple and node.weight < occurences[i].weight:
+            occurences.insert(i, node)
             break
-        elif type(sorted_occurences[i]) == tuple and node.weight < sorted_occurences[i][1]:
-            sorted_occurences.insert(i, node)
-            node = None
+        elif type(occurences[i]) == tuple and node.weight < occurences[i][1]:
+            occurences.insert(i, node)
             break
         i += 1
     # In case node hasn't been add to the list, append it.
-    if node != None:
-        sorted_occurences.append(node)
+    occurences.append(node)
 
+# ---------------- Huffman tree builder -----------------
+def create_huffman_tree():
+    text = get_file_content()
+    occurences = count_and_sort_occurences(text)
+    node = None
 
-print("END: ")
-print(node)
+    # As we combine all elements into 1 tree, length will decrease
+    while len(occurences) > 1:
+        # Pop and combine first two elements into a node
+        left_child = occurences.pop(0)
+        right_child = occurences.pop(0)
+
+        node = create_node(left_child, right_child)
+
+        # Append node to the occurences list at the right position
+        insert_node(node, occurences)
+    return node
+
+# ---------------- Main -----------------
+if __name__ == "__main__":
+    tree = create_huffman_tree()
+    print(tree)
